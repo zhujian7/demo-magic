44ffd7809178a00a6f207e2b2c6dbf4778cb3782
zhujian <jiazhu@redhat.com>
2025-03-13 17:12:06 +0800

    Add a common func
    Signed-off-by: zhujian <jiazhu@redhat.com>

---
+++ common.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+
+# some scripts copy from ianzhang
+function comment() {
+    p "# $1"
+}
+
+function commentNoWait() {
+    printf "$GREY# %s \n" "$1"
+}
+
+function waitFor() {
+    local namespaceArgs=""
+    local kubeconfigArgs=""
+    local kind=$1
+    local name=$2
+    local ns=$3
+    local kubeconfig=$4
+    if [ -n "$ns" ]; then
+        namespaceArgs=" -n $ns"
+    fi
+    if [ -n "$kubeconfig" ]; then
+        kubeconfigArgs=" --kubeconfig $kubeconfig"
+    fi
+
+    echo "kubectl get $kind $name $namespaceArgs $kubeconfigArgs --no-headers --ignore-not-found=true"
+    matched=$(kubectl get $kind $name $namespaceArgs $kubeconfigArgs --no-headers --ignore-not-found=true)
+
+    while [ -z "$matched" ]; do
+        matched=$(kubectl get $kind $name $namespaceArgs $kubeconfigArgs --no-headers --ignore-not-found=true)
+        echo "Waiting for $kind ($ns/$name) to be created"
+        sleep 3
+    done
+    # echo "$kind ($ns/$name) was created"
+}
+
+function switchKubeContext() {
+    local context=$1
+    commentNoWait "kubectl config use-context ${context}"
+    kubectl config use-context ${context}
+}
