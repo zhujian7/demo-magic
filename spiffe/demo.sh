f504f5a500dd51b12d3ed5f4883b4ccb17858ca7
zhujian <jiazhu@redhat.com>
2025-03-17 22:45:18 +0800

    Add a demo about using kubectl oidc login for keycloak
    Signed-off-by: zhujian <jiazhu@redhat.com>

--- spiffe/demo.sh
+++ spiffe/demo.sh
@@ -40,12 +40,17 @@ clear
 # export KUBECONFIG
 
 source ${DEMO_DIR}/config/variable.txt
+export $(cut -d= -f1 ${DEMO_DIR}/config/variable.txt)
 
 HUB_KUBECONFIG="${KUBECONFIG:=${DEMO_DIR}/credentials/ocm_hub.kubeconfig}"
 KIND_KUBECONFIG="${KIND_KUBECONFIG:=${DEMO_DIR}/manifests/credentials/kind.kubeconfig}"
-KEYCLOAK_CLIENT_KUBECONFIG="${KEYCLOAK_CLIENT_KUBECONFIG:=${DEMO_DIR}/manifests/credentials/keycloak_client.kubeconfig}"
 OIDC_PROVIDER_CA_PATH="${DEMO_DIR}/manifests/credentials/oidc_ca.crt"
 CLIENT_TOKEN_PATH="${DEMO_DIR}/manifests/credentials/client_token"
+
+KEYCLOAK_KIND_KUBECONFIG="${KEYCLOAK_KIND_KUBECONFIG:=${DEMO_DIR}/manifests/credentials/keycloak/keycloak_kind.kubeconfig}"
+KEYCLOAK_CLIENT_KUBECONFIG="${KEYCLOAK_CLIENT_KUBECONFIG:=${DEMO_DIR}/manifests/credentials/keycloak/keycloak_client.kubeconfig}"
+KEYCLOAK_CERT_PATH="${DEMO_DIR}/manifests/credentials/keycloak/certificate.pem"
+KEYCLOAK_KEY_PATH="${DEMO_DIR}/manifests/credentials/keycloak/key.pem"
 echo "HUB_KUBECONFIG: ${HUB_KUBECONFIG}"
 echo "KIND_KUBECONFIG: ${KIND_KUBECONFIG}"
 # echo "KEYCLOAK_CLIENT_KUBECONFIG: ${KEYCLOAK_CLIENT_KUBECONFIG}"
@@ -202,12 +207,12 @@ function install_keycloak() {
     echo 'Ensure the keycloak operator is installed in the "keycloak" namespace, if not you can install it from the ocp console'
     # p "Create the keycloak key and cert"
     # envsubst <${DEMO_DIR}/manifests/keycloak/openssl.cnf | openssl req -new -newkey rsa:2048 -days 365 -nodes -x509 \
-    #     -keyout ${DEMO_DIR}/manifests/credentials/keycloak/key.pem -out ${DEMO_DIR}/manifests/credentials/keycloak/certificate.pem -config -
+    #     -keyout ${KEYCLOAK_KEY_PATH} -out ${KEYCLOAK_CERT_PATH} -config -
 
     pe "envsubst <${DEMO_DIR}/manifests/keycloak/keycloak.yaml | kubectl apply -f -"
     pe "kubectl create secret tls keycloak-tls-secret -n keycloak \
-        --cert ${DEMO_DIR}/manifests/credentials/keycloak/certificate.pem \
-        --key ${DEMO_DIR}/manifests/credentials/keycloak/key.pem"
+        --cert ${KEYCLOAK_CERT_PATH} \
+        --key ${KEYCLOAK_KEY_PATH}"
 
     echo "--------------------------------------------------"
     echo "Keycloak installed"
@@ -233,13 +238,51 @@ function configure_keycloak_as_oidc_provider() {
         exit 1
     fi
     p "Configure keycloak as the oidc provider, keycloak realm name: ${KEYCLOAK_REALM_NAME}, oidc client id: ${KEYCLOAK_OIDC_CLIENT_ID}"
-    pei "oc --kubeconfig=${KEYCLOAK_CLIENT_KUBECONFIG} create configmap -n openshift-config openid-ca-keycloak --from-file=ca.crt=${DEMO_DIR}/manifests/credentials/keycloak/certificate.pem"
-    pei "oc --kubeconfig=${KEYCLOAK_CLIENT_KUBECONFIG} create secret generic -n openshift-config openid-client-secret-keycloak --from-literal=clientSecret=${KEYCLOAK_OIDC_CLIENT_SECRET}"
+    pei "oc --kubeconfig=${KEYCLOAK_CLIENT_KUBECONFIG} create configmap -n openshift-config openid-ca-keycloak --from-file=ca.crt=${KEYCLOAK_CERT_PATH}"
+    p 'oc --kubeconfig=${KEYCLOAK_CLIENT_KUBECONFIG} create secret generic -n openshift-config openid-client-secret-keycloak --from-literal=clientSecret=${KEYCLOAK_OIDC_CLIENT_SECRET}'
+    oc --kubeconfig=${KEYCLOAK_CLIENT_KUBECONFIG} create secret generic -n openshift-config openid-client-secret-keycloak --from-literal=clientSecret=${KEYCLOAK_OIDC_CLIENT_SECRET}
     pei "envsubst <${DEMO_DIR}/manifests/keycloak/oauth.yaml | oc --kubeconfig=${KEYCLOAK_CLIENT_KUBECONFIG} apply -f -"
 
     p "Check the oauth configure status"
     pei "oc --kubeconfig=${KEYCLOAK_CLIENT_KUBECONFIG} get oauth cluster -oyaml"
     pei "oc --kubeconfig=${KEYCLOAK_CLIENT_KUBECONFIG} get clusteroperators.config.openshift.io authentication -oyaml"
+}
+
+function set_up_keycloak_oidc_plugin_client() {
+    set_up_keycloak_client
+
+    echo '6. create client scope "groups", enable "Include in token scope"'
+    echo '7. create a mapper for the client scope "groups", "Configure a new mapper" -> "Group Membership", "Token Claim Name" should be "groups", enable "Add to access token", disable "Full group path"(otherwise, the groups you got would be "/k8s-admins")'
+    echo '8. add "http://localhost:8000/*" into the the "Valid redirect URIs" for the keycloak client created at step 3'
+    echo '9. enable the "groups" client scope for the client'
+    echo '10. create a group "k8s-admins"'
+    echo '11. join the user created at step 4 to the group "k8s-admins"'
+}
+
+function create_keycloak_kind_cluster() {
+    pe "envsubst < ${DEMO_DIR}/manifests/kind/cluster-config-keycloak.yaml | kind create cluster --name keycloak-client \
+       --kubeconfig=${KEYCLOAK_KIND_KUBECONFIG} --config -"
+}
+
+function set_keycloak_kubeconfig() {
+    if [ -z "${KEYCLOAK_OIDC_CLIENT_SECRET}" ]; then
+        echo "KEYCLOAK_OIDC_CLIENT_SECRET is not set or empty"
+        exit 1
+    fi
+
+    p "Grant permissions for the keycloak group"
+    pei "envsubst <${DEMO_DIR}/manifests/kind/keycloak-user-binding.yaml | kubectl --kubeconfig=${KEYCLOAK_KIND_KUBECONFIG} --context kind-keycloak-client apply -f -"
+
+    p "Construct a kubeconfig using the oidc-login plugin"
+    kubectl --kubeconfig=${KEYCLOAK_KIND_KUBECONFIG} --context kind-keycloak-client \
+        config set-credentials keycloak-oidc-login --exec-api-version=client.authentication.k8s.io/v1beta1 \
+        --exec-command=kubectl --exec-arg=oidc-login --exec-arg=get-token \
+        --exec-arg=--oidc-issuer-url=https://${KEYCLOAK_SERVER}/realms/${KEYCLOAK_REALM_NAME} \
+        --exec-arg=--oidc-client-id=ocp-test --exec-arg=--oidc-extra-scope="groups email openid" \
+        --exec-arg=--oidc-client-secret=${KEYCLOAK_OIDC_CLIENT_SECRET} --exec-arg=--insecure-skip-tls-verify
+
+    pei "kubectl --kubeconfig=${KEYCLOAK_KIND_KUBECONFIG} --context kind-keycloak-client config set-context keyclock-oidc --cluster=kind-keycloak-client --user=keycloak-oidc-login"
+    pe "kubectl --kubeconfig=${KEYCLOAK_KIND_KUBECONFIG} --context kind-keycloak-client get ns"
 }
 
 function main() {
